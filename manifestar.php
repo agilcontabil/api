a1e2d60869a19c3a2891c1ec080dd27b78e74041
Bill Barsch <billbarsch@gmail.com>
2021-02-11 11:40:42 -0300

    fix.php

--- manifestar.php
+++ manifestar.php
@@ -3,17 +3,17 @@ $urlIntegracao    = 'http://api.agilcontabil.net/nfe/manifestar';
 $dados['usuario'] = 'teste';
 $dados['senha']   = 'teste';
 //para usar certificado A1 apenas descomentar os dados abaixo
-$dados['certificadoDigital'] = bin2hex(file_get_contents("./certificado.pfx"));
-$dados['senhaCertificadoDigital'] = '1234';
+$dados['certificadoDigital'] = bin2hex(file_get_contents("D:\\laragon\\www\\maisagil\\public\\modulos\\api\\39678831569137928.pfx"));
+$dados['senhaCertificadoDigital'] = '23101981';
 
 $dados["dados"] = bin2hex(json_encode([
     "evento" => "210200", //210200 - Confirmação da Operação | 210210 - Ciência da Operação | 210220 - Desconhecimento da Operação | 210240 - Operação não Realizada
-    "razaoSocial" => "Sottile",
-    "cnpj" => "43454545456556",
+    "razaoSocial" => "Hellen Confeccoes",
+    "cnpj" => "26.888.917/0001-68",
     "ambiente" => "1",
     "modelo" => "55",
-    "ufEmitente" => "GO",
-    "chave" => "52200716890284000179650100000001031491472119",
+    "ufEmitente" => "TO",
+    "chave" => "17210218958029000146550010000170681111770138",
     "justificativa" => "Recuso devido não tem ciência da nota..."
 ]));
 
