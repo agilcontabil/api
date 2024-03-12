5198b9ad53249f6cfb49dd2f2b66286582be38b6
Bill Barsch <billbarsch@gmail.com>
2024-03-12 18:30:19 -0300

    fix.php

--- emitirNfse.php
+++ emitirNfse.php
@@ -84,6 +84,7 @@ $nfse = [
         "municipio"          => "3106200",
         "nomeMunicipio"      => "Cidade teste",
         "uf"                 => "MG",
+        "codigoPais"         => "1058",
         "pais"               => "Brasil",
         "cep"                => "30411107",
         "razaoSocial"        => "Cliente Teste",
